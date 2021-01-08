43f4a4501cb073f0794034af37a1ae034c9937ed
ZKrakus <ZKrakus@gmail.com>
2021-01-08 14:34:21 -0500

    Added highlight color to Nav component.

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,14 +1,39 @@
 import React from "react";
 
 export default class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedNav: "Top",
+    };
+
+    // This matters when we are passing the invocation to another component
+    this.updateNav = this.updateNav.bind(this);
+  }
+
+  updateNav(selectedNav) {
+    this.setState({ selectedNav: selectedNav });
+  }
+
   render() {
-    const navList = ["Top", "New"];
+    const navs = ["Top", "New"];
 
     return (
       <ul className="flex-center">
-        {navList.map((navItem) => (
-          <li key={navItem}>
-            <button className="btn-clear nav-link">{navItem}</button>
+        {navs.map((nav) => (
+          <li key={nav}>
+            <button
+              className="btn-clear nav-link"
+              style={
+                nav === this.state.selectedNav
+                  ? { color: "rgb(187,46, 31)" }
+                  : null
+              }
+              onClick={() => this.updateNav(nav)}
+            >
+              {nav}
+            </button>
           </li>
         ))}
       </ul>
